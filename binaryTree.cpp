ab7820c2f60e66b643add684504746a359f5302a
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-26 15:54:56 +0700

    step 7

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -85,10 +85,16 @@ public:
                 // 6b: Exit
                 return;
             }
-           
+            // Step 7: if the value in the data field of the new node is greater than that of the
+            else if (x > parent->info)
+            {
+                // 7a: Make the right child of parent point to the new node
+                parent->rightchild = newNode;
+
+                // 7b: Exit
+                return;
+            }
         }
     }
 
    
-};
-
