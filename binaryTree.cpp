7ab8ecfc49f3de0ae444e4d286a654ba48de5a89
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-26 15:57:45 +0700

    memasukan fungsi postorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -134,6 +134,21 @@ public:
         preorder(ptr->rightchild);
     }
 
-   
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        
+       
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; // parent
+    }
 };
 
